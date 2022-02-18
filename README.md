3d8f52cb5c3d67e57ac6dbe8dc31739606afc047
NotCreative21 <seanray410@gmail.com>
2022-02-17 21:58:06 -0500

    rewrite of entire thing

--- README.md
+++ README.md
@@ -1,13 +1,23 @@
-## todo
+### todo
 
-This tiny program is one of the first things I've coded in rust that I actually use. Only tested to work on linux because that is what I [use](https://github.com/NotCreative21/dotfiles).
+This was the first thing I created in rust.
 
-To list out todos simply type `todo` in the terminal, to add a todo type `todo arg`, to remove a todo type `todo rm x` where x is the number which corresponds with the todo you'd like to remove.
+By default, a global todo file will be created and accessed in `~/.config/todo` 
+
+A seperate todo file can be created in each directory for individual things or projects
+
+
+#### commands
+* todo        => list todos
+* todo new    => create todo in current directory
+* todo rm x   => remove todo in xth index (1 indexed)
+* todo rm all => remove todo in current directory
+* todo rm x-y => remove all todos in range x-y
 
 ### installing
 
-Edit the variable `file` on line 31 of main.rs to match where you'd like the todo file to be and what you'd like it to be named. (In this case the file name is set to .todo)
+```
+cargo build --release
+sudo cp target/release/todo /usr/local/bin
 
-Next you need to save and build, to build simply run `cargo build --release`. 
-
-Finally, copy the todo binary from ./target/release to a folder that is in your path.
+```
--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,151 @@
-use std::fs::File;
-use std::io::{Read, Write};
+use std::env;
+use std::fs::{read_to_string, remove_file, File};
+use std::io::Write;
+use std::path::PathBuf;
+use dirs::home_dir;
+use std::fs::OpenOptions;
 
-fn fwrite(file_path:&str, input_args:Vec<String> ) { 
-    let mut todo_file = File::create(&file_path).expect("create failed");
-    todo_file.write(&input_args[0].as_bytes()).expect("unable to write");  
-    //println!("Hello, world! {:?}", args[1]);
+fn fwrite(file_path: &str, input_args: Vec<String>) -> std::io::Result<()> { 
+    let mut todo_file = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .open(file_path)
+        .unwrap();
+    todo_file.write_all(
+            ("\n".to_owned() + &input_args.join(" ")
+         ).as_bytes())?;
+    Ok(())
 }
 
-fn read(file_path:&str) -> String {
-    let mut todo_opened = File::open(&file_path).expect("Unable to open");
-         
-    let mut todo_contents:String = String::new();
-    todo_opened.read_to_string(&mut todo_contents).expect("failed to read file");
-    todo_contents
-}
-
-fn print_todo(file_path:&str) {
-    let file_path:String = read(&file_path);
-    let todo_vec:Vec<&str> = file_path.split("\n").collect();
-    let mut num:u8 = 1;
+fn print_todo(file_path: &str) -> std::io::Result<()> {
+    let file_path: String = read_to_string(file_path)?;
+    let todo_vec: Vec<&str> = file_path.split("\n").collect();
+    let mut num: u8 = 1;
     for i in todo_vec.iter() {
         if i.len() > 0 {
             println!("{:>2}. {}", num, i);
             num += 1;
         }
     }
+    Ok(())
 }
 
-fn main() {
-    let file:&str = "/home/sean/Desktop/.todo";
-    let mut args:Vec<String> = std::env::args().collect();
+fn get_data(file_path: &str) -> Vec<String> {
+    let data = match read_to_string(file_path) {
+        Ok(v) => v,
+        Err(_) => {
+            eprintln!("could not read todo");
+            std::process::exit(1);
+        },
+    };
+    let mut values = Vec::new();
+    data.split("\n").for_each(|x| values.push(x.to_string()));
+    values
+}
+
+fn write_replace(file_path: &str, contents: String) -> std::io::Result<()> {
+    let mut todo = match OpenOptions::new()
+        .write(true)
+        .open(file_path) 
+    {
+        Ok(v) => v,
+        Err(_) => {
+            eprintln!("failed to open todo list, creating file");
+            let _ = File::create(file_path);
+            OpenOptions::new()
+                .write(true)
+                .open(file_path)
+                .expect("failed to open file")
+        }
+    };
+    let contents = contents.as_bytes();
+    todo.set_len(contents.len() as u64)?;
+    todo.write_all(contents)?;
+    Ok(())
+}
+
+fn parse_range(arg: String) -> (usize, usize) {
+    if arg.len() < 3 { std::process::exit(1); }
+
+    let nums: Vec<&str> = arg.split("-").collect();
+
+    let result = match (nums[0].parse(), nums[1].parse()) {
+        (Ok(v), Ok(e)) => (v, e),
+        _ => {
+            eprintln!("invalid range!");
+            std::process::exit(1);
+        }
+    };
+
+    result
+}
+
+fn remove_data(file: &str, removal: usize) -> std::io::Result<()> {
+    let mut data = get_data(file);
+    if removal < 1 || removal > data.len() {
+        eprintln!("invalid length to remove");
+        std::process::exit(1); 
+    }
+    data.remove(removal);
+
+    write_replace(file, data.join("\n"))?;
+    Ok(())
+}
+
+fn main() -> std::io::Result<()> {
+    let file: &str = &(
+        home_dir()
+            .unwrap()
+            .to_str()
+            .unwrap()
+            .to_owned() + 
+            "/.config/todo"
+        );
+    let file = match PathBuf::from("./todo").exists() {
+        true => "./todo",
+        false => file
+    };
+    let args: Vec<String> = env::args().skip(1).collect();
 
     // if there is nothing to add, print out the current todo
-    if args.len() == 1 {
-        //let mut todos = &read(&file).split("\n");
-        print_todo(&file);     
+    if args.len() == 0 {
+        print_todo(&file)?;
         std::process::exit(0);
     }
 
-    args.remove(0);
+    match args[0].as_str() {
+        "rm" => {
+            if args.len() < 2 { std::process::exit(1); }
+            let removal: usize = match args[1].parse() {
+                Ok(v) => v,
+                Err(_) => {
+                    match args[1].as_str() {
+                        "all" => {
+                            remove_file(file)?;
+                            println!("removed todo in current directory");
+                            std::process::exit(0);
+                        },
+                        _ => {
+                            let (start, end) = parse_range(args[1].clone());
 
-    if args[0] == "rm" {
-        let mut split_content:Vec<String> = Vec::with_capacity(0); 
-            
-        let file_path:String = read(&file);
-        let old_content:Vec<&str> = file_path.split("\n").collect();
-
-        for i in old_content {
-            split_content.push(String::from(i));
+                            let range = end - start;
+                            let mut data = get_data(file);
+                            for _ in 0..(range + 1) {
+                                data.remove(start);
+                            }
+                            write_replace(file, data.join("\n"))?;
+                            std::process::exit(0);
+                        }
+                    }
+                }
+            };
+            remove_data(file, removal)?;
+        },
+        "new" => {
+            File::create(file)?;
+            println!("created new todo file at {}", file);
         }
-
-        let x:u8 = args[1].parse().unwrap();
-
-        split_content.remove((x-1) as usize);
- 
-        let new_todo:Vec<String> = vec![split_content.join("\n")];
-
-        fwrite(&file, new_todo);
-        print_todo(&file);
-
-        std::process::exit(0);
-    } 
-    let mut new_contents:Vec<String> = Vec::with_capacity(0);
-    //let new_content:String = args.join(" "); 
-    new_contents.push(read(&file) + "\n" + &args.join(" ")); 
-    fwrite(&file, new_contents); 
-
-    print_todo(&file);
-
-    std::process::exit(0);
+        _ => fwrite(&file, args)? 
+    }
+    Ok(())
 }
